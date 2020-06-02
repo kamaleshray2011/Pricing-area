ea43b090783f33022cfc12d0cd694aeb8f384d3e
kamaleshray2011 <kamaleshray2011@gmail.com>
2020-06-02 13:57:26 +0600

    Pricing section

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        dots: false,
+        smartSpeed: 500,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
